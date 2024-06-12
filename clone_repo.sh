0262fb8323be619c7620e3a2c4c00cd0e6cd3a89
Paul Veal <vealtech@icloud.com>
2024-06-12 16:50:50 +0100

    Refactored to use a function and allow for a single repo to be cloned

--- clone_repo.sh
+++ clone_repo.sh
@@ -1,36 +1,81 @@
 #!/bin/zsh
-usage() { echo "Usage: clone_repo.sh <github org>"; exit; }
 
-if [ "$1" = "" ]
-then
+# Shell script to clone repositories from a GitHub organization using 'gh' CLI tool.
+# The script allows cloning all repositories from the organization or a specific repository based on input arguments.
+# It also supports updating existing clones by pulling changes from the remote repository.
+
+# Function to display usage information when script is run without required arguments
+usage() {
+  echo "Usage: clone_repo.sh <github org> [<github repo>]"
+  exit
+}
+
+# Flag to indicate if single repository cloning is specified
+singlerepo=false
+
+# Check if script is run without any arguments
+if [ "$1" = "" ]; then
   usage
   exit
+# Check if only the organization name is provided
+elif [ "$2" = "" ] && [ "$1" != "" ]; then
+  echo "All repos being cloned for org: $1"
+else
+  # Indicates that a specific repository will be cloned
+  echo "Single specific repo being cloned: $1/$2"
+  singlerepo=true
 fi
 
-# # 1. Login with gh for private repos, and follow prompts
+# Authenticates with GitHub using 'gh' CLI
 # gh auth login
 
-myorgname=$1  # Set the organization name to 'SmartSkies'
+# Store the organization name provided as input
+myorgname=$1
 
-total_repos=$(gh repo list $myorgname --limit 1000 | wc -l | awk '{$1=$1};1')  # Count the total number of repositories in the organization
-current_repo=0  # Initialize the current repository counter
+# Get the total number of repositories in the organization
+total_repos=$(gh repo list $myorgname --limit 1000 | wc -l | awk '{$1=$1};1')
 
-# Loop through each repository within the organization
-gh repo list $myorgname --limit 1000 | while read -r repo _; do
-    current_repo=$((current_repo + 1))  # Increment the current repository counter
-    echo "Cloning repository" $current_repo "out of" $total_repos: $repo  # Display the progress of cloning the repository
+current_repo=0
 
-    repo=${repo#$myorgname/}  # Remove 'orgname/' prefix from the repository name
+# Function to clone a repository using 'gh' CLI
+# Parameters:
+#   $1 - Repository name
+#   $2 - Current repository count
+#   $3 - Total number of repositories
+clone_repo() {
+  # Repository name
+  local repo=$1
+  # Current repository count
+  local current_repo=$2
+  # Total number of repositories
+  local total_repos=$3
 
-    gh repo clone "$repo" -- -q 2>/dev/null || (
-        cd "$repo"
-        # Handle case where local checkout is on a non-main/master branch
-        # - ignore all errors because some repos may have zero commits,
-        # so no main or master
-        set +e
-        git checkout -q main 2>/dev/null
-        git checkout -q master 2>/dev/null
-        git pull -q
-        set -e
-    )
-done
+  echo "Cloning repository $current_repo out of $total_repos: $repo"
+
+  # Clone the repository using 'gh repo clone' command
+  gh repo clone "$repo" -- -q 2>/dev/null || (
+    # Extract the repository directory name
+    repodir=$(echo "$repo" | sed -E 's/.*\/([^\/]*)/\1/' | sed -E "s/${myorgname}\///I")
+
+    echo "Updating: $repodir"
+    cd "$repodir" || exit
+
+    # Try to update the cloned repository
+    set +e
+    git checkout -q main 2>/dev/null
+    git checkout -q master 2>/dev/null
+    git pull -q
+    set -e
+  )
+}
+
+# Check if single repository cloning is specified
+if [ "$singlerepo" = true ]; then
+  clone_repo "$myorgname/$2" 1 1
+else
+  # Loop through all repositories in the organization and clone/update them
+  gh repo list $myorgname --limit 1000 | while read -r repo _; do
+    current_repo=$((current_repo + 1))
+    clone_repo "$repo" "$current_repo" "$total_repos"
+  done
+fi
