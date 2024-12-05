a540f8b5f30d065a2a402011167a124f0abe2e20
Paul Veal <paul@veal.cc>
2024-12-05 15:11:10 +0000

    Add dev_scripts to path

--- get_devscripts.sh
+++ get_devscripts.sh
@@ -11,3 +11,9 @@ mv dev_scripts-main dev_scripts
 
 # Clean up the ZIP file
 rm dev_scripts.zip
+
+# Add the new dev_scripts directory to the PATH environment variable
+export PATH=$PATH:$(pwd)/dev_scripts
+
+# Print the updated PATH to verify
+echo "Updated PATH: $PATH"
